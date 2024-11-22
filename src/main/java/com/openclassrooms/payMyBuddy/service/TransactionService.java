79367b9fb99a19f9c987676ded3cc79448b3eab2
Astri Brugeilles <astri.brugeilles@gmail.com>
2024-11-22 18:47:47 +0100

    Service layer implemented for needed crud methods

--- src/main/java/com/openclassrooms/payMyBuddy/service/TransactionService.java
+++ src/main/java/com/openclassrooms/payMyBuddy/service/TransactionService.java
@@ -1,23 +1,27 @@
 package com.openclassrooms.payMyBuddy.service;
 
 import com.openclassrooms.payMyBuddy.model.Transaction;
+import com.openclassrooms.payMyBuddy.model.User;
 import com.openclassrooms.payMyBuddy.repository.TransactionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class TransactionService {
 
-    @Autowired
-    private TransactionRepository transactionRepository;
+    private final TransactionRepository transactionRepository;
 
-    public Iterable<Transaction> getTransactions() {
-        return transactionRepository.findAll();
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
     }
 
-    public Optional<Transaction> getTransactionById(Integer id) {
-        return transactionRepository.findById(id);
+    public List<Transaction> getAllTransactionsBySender(User sender) {
+        return transactionRepository.findBySender(sender);
+    }
+
+    public void saveNewTransaction(User sender, User receiver, String description, double amount) {
+        Transaction transaction = new Transaction(sender, receiver, description, amount);
+        transactionRepository.save(transaction);
     }
 }
--- src/main/java/com/openclassrooms/payMyBuddy/service/UserService.java
+++ src/main/java/com/openclassrooms/payMyBuddy/service/UserService.java
@@ -2,23 +2,69 @@ package com.openclassrooms.payMyBuddy.service;
 
 import com.openclassrooms.payMyBuddy.model.User;
 import com.openclassrooms.payMyBuddy.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private final Logger logger = Logger.getLogger(UserService.class.getName());
+
+    // vérifier usage
     public Iterable<User> getUsers() {
         return userRepository.findAll();
     }
 
+    // vérifier usage
     public Optional<User> getUser(Integer id) {
         return userRepository.findById(id);
     }
 
+    public void addNewBuddy(String email, User user) {
+        List<User> myBuddies = user.getBuddies();
+        User newBuddy = userRepository.findByEmail(email);
+        if (newBuddy != null) {
+            // setting the buddies list if null
+            if (myBuddies == null) {
+                user.setBuddies(new ArrayList<>());
+            }
+            // avoid double datas
+            if (!myBuddies.contains(newBuddy)) {
+                myBuddies.add(newBuddy);
+                newBuddy.getBuddies().add(user);
+                userRepository.save(user);
+                userRepository.save(newBuddy);
+            }
+        }
+        logger.warning("Nobody found with email: " + email);
+    }
+
+    List<User> getAllMyBuddies(User sender) {
+        return sender.getBuddies();
+    }
+
+    List<String> getAllMyBuddyNames(User sender) {
+        List<String> buddiesNames = new ArrayList<>();
+        for (User buddy : getAllMyBuddies(sender)) {
+            buddiesNames.add(buddy.getUsername());
+        }
+        return buddiesNames;
+    }
+
+    User updateUser(String username, String email, String password, User user) {
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(password);
+        return userRepository.save(user);
+    }
 }
