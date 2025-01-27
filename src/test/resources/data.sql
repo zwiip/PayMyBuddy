710506c6024f8b3c9f0281626bc85cd7cefdb6c0
Astri Brugeilles <astri.brugeilles@gmail.com>
2025-01-27 21:44:24 +0100

    ajusting the script to avoid a bug (duplicates)

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -8,8 +8,8 @@ INSERT IGNORE INTO assoc_connections (user_id, connection_id) VALUES
 ((SELECT id FROM users WHERE email = 'anne@shirley.com'), (SELECT id FROM users WHERE email = 'diana@barry.com')),
 ((SELECT id FROM users WHERE email = 'matthew@cuthbert.com'), (SELECT id FROM users WHERE email = 'diana@barry.com'));
 
-INSERT IGNORE INTO transaction (sender, receiver, description, amount) VALUES
-((SELECT id FROM users WHERE email = 'anne@shirley.com'), (SELECT id FROM users WHERE email = 'matthew@cuthbert.com'), 'Participation Pique Nique', 20.50), -- Anne vers Matthew
-((SELECT id FROM users WHERE email = 'matthew@cuthbert.com'), (SELECT id FROM users WHERE email = 'diana@barry.com'), 'Remboursement du prêt', 15.00),  -- Matthew vers Diana
-((SELECT id FROM users WHERE email = 'matthew@cuthbert.com'), (SELECT id FROM users WHERE email = 'anne@shirley.com'), 'Achat livres', 12.25), -- Matthew vers Anne
-((SELECT id FROM users WHERE email = 'diana@barry.com'), (SELECT id FROM users WHERE email = 'anne@shirley.com'), 'Achat fleurs', 30.00); -- Diana vers Anne
+INSERT IGNORE INTO transaction (id, sender, receiver, description, amount) VALUES
+(1, (SELECT id FROM users WHERE email = 'anne@shirley.com'), (SELECT id FROM users WHERE email = 'matthew@cuthbert.com'), 'Participation Pique Nique', 20.50), -- Anne vers Matthew
+(2, (SELECT id FROM users WHERE email = 'matthew@cuthbert.com'), (SELECT id FROM users WHERE email = 'diana@barry.com'), 'Remboursement du prêt', 15.00),  -- Matthew vers Diana
+(3, (SELECT id FROM users WHERE email = 'matthew@cuthbert.com'), (SELECT id FROM users WHERE email = 'anne@shirley.com'), 'Achat livres', 12.25), -- Matthew vers Anne
+(4, (SELECT id FROM users WHERE email = 'diana@barry.com'), (SELECT id FROM users WHERE email = 'anne@shirley.com'), 'Achat fleurs', 30.00); -- Diana vers Anne
