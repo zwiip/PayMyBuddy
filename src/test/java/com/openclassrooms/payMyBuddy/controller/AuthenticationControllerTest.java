a2a52c44d5165a28210e8a4172b5f99ab6e5cfee
abrugeilles <abrugeilles@ardans.fr>
2025-01-27 14:45:30 +0100

    writing integration tests

---
+++ src/test/java/com/openclassrooms/payMyBuddy/controller/AuthenticationControllerTest.java
@@ -0,0 +1,52 @@
+package com.openclassrooms.payMyBuddy.controller;
+
+import com.openclassrooms.payMyBuddy.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+//@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testGetLogin() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+
+    @Test
+    void testGetSignin_Success() throws Exception {
+        mockMvc.perform(get("/signin"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("signin"));
+    }
+
+    @Test
+    void testGetLogout_Success() throws Exception {
+        mockMvc.perform(get("/logout"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    void testPostSignin_Success() throws Exception {
+        User user = new User("Marilla", "marilla@cuthbert.com", "avonlea");
+
+        mockMvc.perform(post("/signin").with(csrf()).flashAttr("user", user))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login"));
+    }
+}
